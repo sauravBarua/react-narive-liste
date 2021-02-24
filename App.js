6236b040ee1b61b34b4b9e6801ad6bce6682500b
sauravBarua <myallmailofficial@gmail.com>
2021-02-24 17:31:06 +0100

    Update

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useEffect, useState } from 'react';
 
-const List = ()=>{
+const List = () => {
   const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-    const getCoutries = async()=>{
-      const data = await fetch("https://restcountries.eu/rest/v2/all?fildes=nane");
+  useEffect(() => {
+    const getCoutries = async () => {
+      const data = await fetch("https://restcountries.eu/rest/v2/all");
       const jsonData = await data.json();
-      console.log(jsonData);
+      console.log("jsonData", jsonData);
       setCountries(jsonData);
     }
     getCoutries();
-  },[])
+    console.log("countries", countries);
+  }, [])
 
-  return(
+  return (
     <View>
       <Text>List of Countries</Text>
-     
+      <FlatList
+        data={countries}
+        renderItem=
+        {
+          ({ item }) =>
+          (
+            <View >
+              <Text>Country name: {item.name} <br /> Capital : {item.capital} </Text>
+            </View>
+          )
+        }
+      />
+
     </View>
   )
 }
